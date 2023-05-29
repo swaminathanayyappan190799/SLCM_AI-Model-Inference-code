75c9ef6c2a45b965a919350908c217dee2b4834b
swaminathan.ayyappan <swaminathan.ayyappan@nuvento.net>
2023-05-29 11:38:45 +0530

    execution stopping for other and wheat commodity

--- python code/main.py
+++ python code/main.py
@@ -11,7 +11,6 @@ try:
     classifier_model_results = classifier_model_obj.run_classifier_inference()
     try:
         classifier_pop_up_app = QApplication([])
-        # Create a message box dialog
         classifier_pop_up = QMessageBox()
         classifier_pop_up.setIcon(QMessageBox.Information)
         classifier_pop_up.setText(f"The given input image is classified as : {classifier_model_results}")
@@ -19,28 +18,28 @@ try:
         classifier_pop_up.setStandardButtons(QMessageBox.Ok)
         ok_button = classifier_pop_up.button(QMessageBox.Ok)
         ok_button.clicked.connect(classifier_pop_up.close)
-        # Show the message box
         classifier_pop_up.show()
-        # Run the application event loop
         classifier_pop_up_app.exec_()
     except:
         logger.error("Error in showing the classifier model results")
-    detection_model_obj = detection_inference(input_image=input_image, commodity_configuration=commodity_config[classifier_model_results.lower()], defined_color_scheme=color_scheme)
-    annotated_img_path, grain_counts = detection_model_obj.run_inference()
-    try:
-        detection_popup_app = QApplication([])
-        detection_popup = QMessageBox()
-        detection_popup.setIcon(QMessageBox.Information)
-        detection_popup.setText(f"Detection results are saved in : {annotated_img_path} \n \n Grain counts : {grain_counts}")
-        detection_popup.setWindowTitle("Detection model Prediction")
-        detection_popup.setStandardButtons(QMessageBox.Ok)
-        ok_button = detection_popup.button(QMessageBox.Ok)
-        ok_button.clicked.connect(detection_popup.close)
-        # Show the message box
-        detection_popup.show()
-        detection_popup_app.exec_()
-    except:
-        logger.info("Error in showing detection results")
+    if classifier_model_results not in ["Wheat", "Other-Unknown"]:
+        detection_model_obj = detection_inference(input_image=input_image, commodity_configuration=commodity_config[classifier_model_results.lower()], defined_color_scheme=color_scheme)
+        annotated_img_path, grain_counts = detection_model_obj.run_inference()
+        try:
+            detection_popup_app = QApplication([])
+            detection_popup = QMessageBox()
+            detection_popup.setIcon(QMessageBox.Information)
+            detection_popup.setText(f"Detection results are saved in : {annotated_img_path} \n \n Grain counts : {grain_counts}")
+            detection_popup.setWindowTitle("Detection model Prediction")
+            detection_popup.setStandardButtons(QMessageBox.Ok)
+            ok_button = detection_popup.button(QMessageBox.Ok)
+            ok_button.clicked.connect(detection_popup.close)
+            detection_popup.show()
+            detection_popup_app.exec_()
+        except:
+            logger.info("Error in showing detection results")
+    else:
+        logger.info(f"The Classifier model detected as {classifier_model_results}")
 except Exception as ex:
     logger.error(str(ex))
     logger.error("Error in performing inference check the input data !")
